237ad55d6f98fd9fbf0d56cc6c79dd307fcc7e0d
bakermor <bakermor@oregonstate.edu>
2023-05-23 19:07:17 -0500

    added update functionality

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ import mysql.connector
 app = Flask(__name__)
 port_number = 27205
 
-def connect():
+def connect(command, values):
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",
+        password="P4ssw0rd",
         database="340_project"
     )
     mycursor = mydb.cursor()
-    return mycursor
+    mycursor.execute(command, values)
+    mydb.commit()
 
 # index Page
 @app.route("/")
@@ -20,52 +21,49 @@ def index():
     return render_template("index.html")
 
 # venues Page
-@app.route("/venues", methods=["GET", "POST", "PUT", "DELETE"])
+@app.route("/venues", methods=["GET", "POST"])
 def venues():
-    mycursor = connect()
+
     if request.method == "GET":
         pass
 
     if request.method == "POST":
-        pass
+        print(request.form)
+        
+        if request.form["method"] == "put":
+            venue_id = int(request.form["venue_id"])
+            address = request.form["address"]
+            capacity = int(request.form["capacity"])
+            email = request.form["email"]
+            phone_number = request.form["phone_number"]
 
-    if request.method == "PUT":
-        venue_id = request.form["venue_id"]
-        address = request.form["address"]
-        capacity = int(request.form["capacity"])
-        email = request.form["email"]
-        phone_number = request.form["phone_number"]
-
-        command = "UPDATE Venues SET address = %s, capacity = %d, email = %s, phone_number = %s \
-            WHERE venue_id = %d;"
-        mycursor.execute(command, (address, capacity, email, phone_number, venue_id))
-
-    if request.method == "DELETE":
-        pass
+            command = "UPDATE Venues SET address = %s, capacity = %s, email = %s, phone_number = %s WHERE venue_id = %s;"
+            values = (address, capacity, email, phone_number, venue_id)
+            connect(command, values)
 
     return render_template("venues.html")
 
-# venues Page
+# artists Page
 @app.route("/artists")
 def artists():
     return render_template("artists.html")
 
-# venues Page
+# customers Page
 @app.route("/customers")
 def customers():
     return render_template("customers.html")
 
-# venues Page
+# concerts Page
 @app.route("/concerts")
 def concerts():
     return render_template("concerts.html")
 
-# venues Page
+# ticketsales Page
 @app.route("/ticketsales")
 def ticketsales():
     return render_template("ticketsales.html")
 
-# venues Page
+# intersection Page
 @app.route("/concerts_ticketsales")
 def concerts_ticketsales():
     return render_template("concerts_ticketsales.html")
